8a50841d9a75112847146a8dc07c5e1b97a85bae
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-07-15 22:06:58 -0400

    feat: touchable components

--- App.js
+++ App.js
@@ -4,26 +4,32 @@ import {
   Text,
   View,
   FlatList,
+  TouchableOpacity
 } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: 'shaun', key: '1' },
-    { name: 'yoshi', key: '2' },
-    { name: 'mario', key: '3' },
-    { name: 'luigi', key: '4' },
-    { name: 'peach', key: '5' },
-    { name: 'bowser', key: '6' },
-    { name: 'toad', key: '7' },
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'bowser', id: '6' },
+    { name: 'toad', id: '7' },
   ])
-
+  const pressHandler = (id) => {
+    setPeople((prevPeople) => prevPeople.filter(item => item.id !== id))
+  }
   return (
     <View style={styles.container}>
       <FlatList
         numColumns={2}
         data={people}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       />
       {/* <ScrollView>
